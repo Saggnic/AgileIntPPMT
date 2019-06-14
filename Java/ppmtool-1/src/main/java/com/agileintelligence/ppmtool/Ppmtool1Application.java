318ceb901b099af04f3e9d9147a5ac65d27657bb
Saggnic <saggnic1@gmail.com>
2019-06-14 06:37:42 +0530

    my first commit

---
+++ Java/ppmtool-1/src/main/java/com/agileintelligence/ppmtool/Ppmtool1Application.java
@@ -0,0 +1,13 @@
+package com.agileintelligence.ppmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ppmtool1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ppmtool1Application.class, args);
+	}
+
+}
